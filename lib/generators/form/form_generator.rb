35a1d34883116457d3fc542fe9a24955aef59e66
jwald1 <>
2019-05-20 14:19:36 -0400

    update generator USAGE

--- lib/generators/form/form_generator.rb
+++ lib/generators/form/form_generator.rb
@@ -8,7 +8,7 @@ class FormGenerator < Rails::Generators::NamedBase
   argument :attributes, type: :array, default: [], banner: 'attribute attribute'
 
   def create_form_file
-    template 'form.rb.tt', File.join('app', 'forms', class_path, "#{file_name}_form.rb")
+    template 'form.template', File.join('app', 'forms', class_path, "#{file_name}_form.rb")
   end
 
   private
